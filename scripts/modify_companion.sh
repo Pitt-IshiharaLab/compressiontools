251a13668161b9f37f8f0c2f0a7524ac3237f0d3
Keisuke Ishihara <tidio634@gmail.com>
2024-04-15 12:10:26 -0400

    create modfiy_companion.sh

---
+++ scripts/modify_companion.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define function to replace string in file
+replace_string_in_file() {
+    local file="$1"
+    local replacement="$2"
+    local temp_file="${file}.tmp"
+
+    # Use sed to replace "FileName=" with "FileName=<replacement>/"
+    sed "s|FileName=\"|FileName=\"${replacement}/|g" "$file" > "$temp_file"
+
+    # Move temp file to original file
+    mv "$temp_file" "$file"
+}
+
+replace_string_in_file "D:\out\MeasurementResult.companion.ome" "Image"
+replace_string_in_file "D:\out\MeasurementResultMIP.companion.ome" "Projection"
+
+# # Check if two input arguments were provided
+# if [ $# -ne 2 ]; then
+#     echo "Usage: $0 <file> <replacement>"
+#     exit 1
+# fi
+
+# Call the function with the provided arguments
+# replace_string_in_file "$1" "$2"
